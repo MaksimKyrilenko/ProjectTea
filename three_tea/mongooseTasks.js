ed01b138a2a45cb139b70399d12221d532140192
MaksimKyrilenko <mack.curilencko2019@yandex.ru>
2023-12-03 16:43:54 +0300

    Создание схемы

--- three_tea/mongooseTasks.js
+++ three_tea/mongooseTasks.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Tea = mongoose.model('Tea', { name: String });
+const teaSchema = new mongoose.Schema( {name: String} );
+teaSchema.methods.brew = function() {
+    console.log(this.name + ' заварился');
+}
+const Tea = mongoose.model('Tea', teaSchema);
 const welding = new Tea({ name: 'Лист' });
 
 welding.save()
-  .then(() => console.log('Сахар'))
+  .then(() => welding.brew())
   .catch((err) => console.log(err));
