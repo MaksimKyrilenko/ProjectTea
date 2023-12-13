54d74ec93f9bfbb92d4d2dd7e0a22a9db5aef988
KyrilenkoMaksim <mack.curilencko2018@yandex.ru>
2023-12-13 10:34:31 +0300

    Извлечение данных из базы данных

--- three_tea/routes/teas.js
+++ three_tea/routes/teas.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Tea = require("../models/tea").Tea
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с teas');
 });
 
-router.get('/black_tea', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get('/green_tea', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get('/herbal_tea', function(req, res, next) {
-    res.send('respond with a resource');
-});
-
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });    
+router.get("/:nick", async (req, res, next) => {
+    try {
+      const tea = await Tea.findOne({ nick: req.params.nick });
+      console.log(tea);
+      if (!tea) {
+        throw new Error("Нет такого чая");
+      }
+      res.render('tea', {
+        title: tea.title,
+        picture: tea.avatar,
+        desc: tea.desc
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+       
 module.exports = router;
