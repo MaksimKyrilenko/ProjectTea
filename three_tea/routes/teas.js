026c86cca98febcae0c673d23ef2b80008237394
KyrilenkoMaksim <mack.curilencko2018@yandex.ru>
2023-12-13 11:24:07 +0300

    Заполнение меню из базы данных

--- three_tea/routes/teas.js
+++ three_tea/routes/teas.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var Tea = require("../models/tea").Tea
+const express = require('express');
+const router = express.Router();
+const Tea = require("../models/tea").Tea;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с teas');
+router.get('/', (req, res) => {
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с teas');
 });
 
-router.get("/:nick", async (req, res, next) => {
+/* Страница котят */
+router.get('/:nick', async (req, res, next) => {
     try {
-      const tea = await Tea.findOne({ nick: req.params.nick });
-      console.log(tea);
-      if (!tea) {
-        throw new Error("Нет такого чая");
-      }
-      res.render('tea', {
-        title: tea.title,
-        picture: tea.avatar,
-        desc: tea.desc
-      });
+        const [tea, teas] = await Promise.all([
+            Tea.findOne({ nick: req.params.nick }).exec(),
+            Tea.find({}, { _id: 0, title: 1, nick: 1 }).exec()
+        ]);
+
+        if (!tea) throw new Error("Нет такого чая");
+
+        res.render('tea', {
+            title: tea.title,
+            picture: tea.avatar,
+            desc: tea.desc,
+            menu: teas || []
+        });
     } catch (err) {
-      next(err);
+         next(err);
     }
-  });
-       
+});
+
 module.exports = router;
