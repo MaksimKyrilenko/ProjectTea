b1faad06ca5909f48d9fc0670c440e1bc3718860
KyrilenkoMaksim <mack.curilencko2018@yandex.ru>
2023-12-16 12:08:54 +0300

    Создал счётчик посещения страниц сайта

--- three_tea/routes/index.js
+++ three_tea/routes/index.js
@@ -1,20 +1,17 @@
-var express = require('express')
-var router = express.Router()
-var Tea = require("../models/tea").Tea
+const express = require('express');
+const router = express.Router();
+const Tea = require("../models/tea").Tea;
+
 /* GET home page. */
-
-
 router.get('/', async (req, res, next) => {
   try {
-      const menu = await Tea.find({}, { _id: 0, title: 1, nick: 1 }).exec();
-      req.session.greeting = 'Hi!!!'
-      res.render('index', { 
-        title: 'Express',
-        menu: menu 
-      });
- } catch (err) {
-next(err);
-}
+    const menu = await Tea.find({}, { _id: 0, title: 1, nick: 1 });
+    req.session.greeting = "Hi!!!"
+    res.render('index', { title: 'Express', menu:menu, counter:req.session.counter });
+  } catch (err) {
+    next(err);
+  }
 });
+
 
 module.exports = router;
