772ece637a04595d19d78dccf9531e536cbbffd8
KyrilenkoMaksim <mack.curilencko2018@yandex.ru>
2023-12-17 13:49:16 +0300

    Подготовка данных для передачи на сервер

--- three_tea/routes/index.js
+++ three_tea/routes/index.js
@@ -14,5 +14,8 @@ router.get('/', async (req, res, next) => {
 router.get('/logreg', (req, res, next) => {
     res.render('logreg', { title: 'Вход' });
   });
-
+  router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+  });
 module.exports = router;
